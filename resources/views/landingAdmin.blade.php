94d003ac602028e5af26180143152b31c8fd9b7c
sena-kumara <79744931+sena-kumara@users.noreply.github.com>
2021-11-15 18:19:46 +0700

    Update landingAdmin.blade.php

--- resources/views/landingAdmin.blade.php
+++ resources/views/landingAdmin.blade.php
@@ -7,27 +7,33 @@
     <title>Book Store</title>
     <link rel="stylesheet" href={{ asset('css/bootstrap.min.css') }}>
 </head>
-    <{{-- Navbar --}}
+    {{-- Navbar --}}
     <nav class="navbar navbar-expand-md navbar-dark bg-primary">
         <div class="container-fluid">
             <a href="#" class="navbar-brand align-center">Book Store</a>
-            <div class="justify-content-end align-center">
-                <ul class="navbar-nav ">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarManageLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Manage
-                        </a>
-                        <ul class="cropdown-menu" aria-labelledby="navbarManageLink">
-                            <li><a href="#" class="dropdown-item">Book</a></li>
-                            <li><a href="#" class="dropdown-item">Book</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">
-                            <span class="text-white">Login</span>
-                        </a>
-                    </li>
-                </ul>
+            <div class="justify-content-end align-center px-4">
+                <div class="collapse navbar-collapse">
+                    <ul class="navbar-nav ">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle px-2" href="#" id="navbarManageLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <span class="text-white">Manage</span>
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarManageLink">
+                                <li><a href="#" class="dropdown-item">Book</a></li>
+                                <li><a href="#" class="dropdown-item">Genre</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle px-3" href="#" id="navbarAccLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <span class="text-white">Hello ...</span>
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarAccLink">
+                                <li><a href="#" class="dropdown-item">Change Password</a></li>
+                                <li><a href="#" class="dropdown-item">Logout</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </nav>
